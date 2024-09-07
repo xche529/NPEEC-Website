a2845bd5700e66fda88160d4ecbd1ed535ff660c
xche529 <xche529@aucklanduni.ac.nz>
2024-09-08 02:58:16 +1200

    feat: add language system

--- npeec-website/src/App.jsx
+++ npeec-website/src/App.jsx
@@ -1,12 +1,9 @@
 import "./index.css";
-
+import Header from "./components/Header";
 function App() {
   return (
     <div className="App">
-      <h1>NPEEC Website</h1>
-      <div className="bg-blue-500 text-white p-4">
-        <h1 className="text-2xl font-bold">Tailwind CSS is working!</h1>
-      </div>
+      <Header />
     </div>
   );
 }
--- npeec-website/src/components/Header.jsx
+++ npeec-website/src/components/Header.jsx
@@ -1,10 +1,32 @@
+import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
+
 function Header() {
+  const { t, i18n } = useTranslation();
+
+  const [lang, setlang] = useState("zh");
+
+  const handleLanguageChange = () => {
+    if (lang === "zh") {
+      setlang("en");
+    } else {
+      setlang("zh");
+    }
+    i18n.changeLanguage(lang);
+  };
+
   return (
-    <header>
-      <h1>NPEEC</h1>
-      <div className="bg-blue-500 text-white p-4">
-        <h1 className="text-2xl font-bold">Tailwind CSS is working!</h1>
+    <header className="p-4 bg-gray-200 flex" data-testid="header">
+      <div className="flex space-x-4 items-center mt-4 lg:mt-0">
+        <h1 className="font-bold text-3xl lg:text-5xl">NPEEC</h1>
+        <button className="m-{5px}">{t("header.aboutUs")}</button>
+        <button>{t("header.contactUs")}</button>
+        <button onClick={() => handleLanguageChange()}>
+          {t("header.language")}
+        </button>
       </div>
     </header>
   );
 }
+
+export default Header;
