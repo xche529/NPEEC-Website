67ac21a2f3f5a2e3542495e0eb374e64e1be32da
xche529 <xche529@aucklanduni.ac.nz>
2024-09-07 15:22:40 +1200

    feat: add tailwind and header

---
+++ npeec-website/src/App.jsx
@@ -0,0 +1,14 @@
+import "./index.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>NPEEC Website</h1>
+      <div className="bg-blue-500 text-white p-4">
+        <h1 className="text-2xl font-bold">Tailwind CSS is working!</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ npeec-website/src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header>
+      <h1>NPEEC</h1>
+      <div className="bg-blue-500 text-white p-4">
+        <h1 className="text-2xl font-bold">Tailwind CSS is working!</h1>
+      </div>
+    </header>
+  );
+}
